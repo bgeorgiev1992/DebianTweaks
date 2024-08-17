7743cf99b99e3e32c56e47bd64990cae1ecbe46d
Borislav Georgiev <b.a.georgiev@outlook.com>
2024-08-17 18:31:41 +0300

    Update SourcesListConf.sh

--- SourcesListConf.sh
+++ SourcesListConf.sh
@@ -10,10 +10,10 @@ sudo apt update; sudo apt install --yes curl wget apt-transport-https dirmngr
 sudo cp -fi /etc/apt/sources.list /etc/apt/sources.list.backup
 
 sudo cat > /etc/apt/sources.list <<EOF
-deb https://deb.debian.org/debian/ trixie main contrib non-free non-free-firmware
-deb https://deb.debian.org/debian/ trixie-updates main contrib non-free non-free-firmware
-deb https://security.debian.org/debian-security trixie-security main contrib non-free non-free-firmware
-deb https://deb.debian.org/debian trixie-backports main contrib non-free non-free-firmware
+deb http://debian.a1.bg/debian/ trixie main contrib non-free non-free-firmware
+deb http://debian.a1.bg/debian/ trixie-updates main contrib non-free non-free-firmware
+deb http://debian.a1.bg/debian-security trixie-security main contrib non-free non-free-firmware
+deb http://debian.a1.bg/debian trixie-backports main contrib non-free non-free-firmware
 EOF
 
 sudo apt clean; sudo apt autoclean; sudo apt update; sudo apt upgrade -y; sudo apt install -f; sudo apt autoremove -y
