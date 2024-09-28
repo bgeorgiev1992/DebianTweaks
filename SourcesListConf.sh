3c9633150284fa0a2704059ff0c1c5a9051a0e44
Borislav Georgiev <b.a.georgiev@outlook.com>
2024-09-28 11:13:57 +0300

    Update SourcesListConf.sh

--- SourcesListConf.sh
+++ SourcesListConf.sh
@@ -10,12 +10,35 @@ sudo apt update; sudo apt install --yes curl wget apt-transport-https dirmngr
 sudo cp -fi /etc/apt/sources.list /etc/apt/sources.list.backup
 
 sudo cat > /etc/apt/sources.list <<EOF
-deb http://debian.a1.bg/debian/ trixie main contrib non-free non-free-firmware
-deb http://debian.a1.bg/debian/ trixie-updates main contrib non-free non-free-firmware
-deb http://debian.a1.bg/debian-security trixie-security main contrib non-free non-free-firmware
-deb http://debian.a1.bg/debian trixie-backports main contrib non-free non-free-firmware
+deb https://deb.debian.org/debian/ trixie main contrib non-free non-free-firmware
+deb https://deb.debian.org/debian/ trixie-updates main contrib non-free non-free-firmware
+deb https://deb.debian.org/debian-security trixie-security main contrib non-free non-free-firmware
+deb https://deb.debian.org/debian trixie-backports main contrib non-free non-free-firmware
 EOF
 
 sudo apt clean; sudo apt autoclean; sudo apt update; sudo apt upgrade -y; sudo apt install -f; sudo apt autoremove -y
- 
+
+## Add Google Chrome repo
+curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/google-linux_signing_key.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/google-linux_signing_key.gpg] https://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt clean; sudo apt autoclean; sudo apt update; sudo apt upgrade -y; sudo apt install -f; sudo apt autoremove -y
+
+## Add Mozilla Firefox repo
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+
+## Disable auto installation of recommended and suggested packages
+sudo touch /etc/apt/apt.conf.d/99no-recommends
+sudo cat > /etc/apt/apt.conf.d/99no-recommends <<EOF
+APT::Install-Recommends "false";
+APT::Install-Suggests "false";
+EOF
+
+sudo apt clean; sudo apt autoclean; sudo apt update; sudo apt upgrade -y; sudo apt install -f; sudo apt autoremove -y
+
 exit 0
